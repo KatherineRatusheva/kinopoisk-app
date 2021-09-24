eba2b9b9d28d2bb2e487814a1dc4c9921267165a
Kate Ratusheva <k.ratusheva@gmail.com>
2021-09-25 00:37:49 +0300

    fix: fix navbar responsive

--- kinopoisk-react-app/src/components/navTags/index.js
+++ kinopoisk-react-app/src/components/navTags/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { ACTION_TYPES } from '../../constants';
 import './index.css'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const category = ['Боевик', 'Драма', 'Комедия', 'Мелодрама', 'Триллер', 'Семейный', 'Фантастика', 'Фэнтези', 'Мультфильм', 'Ужасы']
 
@@ -17,12 +20,86 @@ const NavbarTags = () => {
             payload: item
         })
     }
-    
+
+    const settings = {
+        speed: 500,
+        slidesToShow: 10,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1500,
+            settings: {
+              slidesToShow: 9,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 1300,
+            settings: {
+              slidesToShow: 8,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 1160,
+            settings: {
+              slidesToShow: 7,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 1000,
+            settings: {
+              slidesToShow: 6,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 850,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 700,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 550,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 400,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          }
+        ]
+    }
+
+
     return (
-        <div className='tags-menu'>
+
+        <Slider className='tags-menu' {...settings}>
             {category.map((item) => 
-            <Link to={`/${item}`} key={item} onClick={() => getSelectCategory(item)}><div className='tag-name'> {item} </div></Link>)}
-        </div>
+            <div><Link to={`/${item}`} key={item} onClick={() => getSelectCategory(item)}><div className='tag-name'> {item} </div></Link></div>)}
+        </Slider>
 
     )
 }
